f087ead453e90051ea6c66976e70a2fe3dc1f672
Matthew Pierce <mpierce@impacttrial.com>
2021-11-12 20:45:14 -0500

    Further testing of continuous deployment via GIT/Apmlify/AWS

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <h1>Version update test...</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <h3>[this space for rent]</h3>
+        </div>
+    );
 }
 
 export default App;
